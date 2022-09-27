f01f92aadaa4fc85f000a5e73c194ba2b9c153a8
Don-Alvin <alvindon41@gmail.com>
2022-09-27 20:42:07 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
 	Ecommerce,
 	Orders,
-	Calender,
+	Calendar,
 	Employees,
 	Stacked,
 	Pyramid,
 	Customers,
 	Kanban,
+	Line,
 	Area,
 	Bar,
 	Pie,
@@ -20,33 +21,49 @@ import {
 	ColorPicker,
 	ColorMapping,
 	Editor,
-	Line,
 } from "./pages";
-
-import { useStateContext } from "./context/ContextProvider";
-
 import "./App.css";
 
+import { useStateContext } from "./contexts/ContextProvider";
+
 const App = () => {
-	const { activeMenu } = useStateContext;
+	const {
+		setCurrentColor,
+		setCurrentMode,
+		currentMode,
+		activeMenu,
+		currentColor,
+		themeSettings,
+		setThemeSettings,
+	} = useStateContext();
+
+	useEffect(() => {
+		const currentThemeColor = localStorage.getItem("colorMode");
+		const currentThemeMode = localStorage.getItem("themeMode");
+		if (currentThemeColor && currentThemeMode) {
+			setCurrentColor(currentThemeColor);
+			setCurrentMode(currentThemeMode);
+		}
+	}, []);
 
 	return (
-		<div>
+		<div className={currentMode === "Dark" ? "dark" : ""}>
 			<BrowserRouter>
 				<div className="flex relative dark:bg-main-dark-bg">
-					<div className="fixed right-4 bottom-4 style={{ z-Index: '1000 }}">
+					<div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
 						<TooltipComponent content="Settings" position="Top">
 							<button
 								type="button"
-								className="text-3x1 p-3 hover:drop-shadow-x1 hover:bg-light-gray text-white"
-								style={{ background: "blue", borderRadius: "50%" }}
+								onClick={() => setThemeSettings(true)}
+								style={{ background: currentColor, borderRadius: "50%" }}
+								className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
 							>
 								<FiSettings />
 							</button>
 						</TooltipComponent>
 					</div>
 					{activeMenu ? (
-						<div className="w-72 fixed dark:bg-secondary-bg bg-white sidebar">
+						<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
 							<Sidebar />
 						</div>
 					) : (
@@ -57,42 +74,44 @@ const App = () => {
 					<div
 						className={
 							activeMenu
-								? "dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full"
-								: "dark:bg-main-bg bg-main-bg min-h-screen flex-2 w-full"
+								? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+								: "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
 						}
 					>
-						<div className="fixed md-static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+						<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
 							<Navbar />
 						</div>
-					</div>
+						<div>
+							{themeSettings && <ThemeSettings />}
 
-					<div>
-						<Routes>
-							{/* Dashboard */}
-							<Route path="/" element={<Ecommerce />} />
-							<Route path="/ecommerce" element={<Ecommerce />} />
+							<Routes>
+								{/* dashboard  */}
+								<Route path="/" element={<Ecommerce />} />
+								<Route path="/ecommerce" element={<Ecommerce />} />
 
-							{/* Pages */}
-							<Route path="/orders" element={<Orders />} />
-							<Route path="/employees" element={<Employees />} />
-							<Route path="/customers" element={<Customers />} />
+								{/* pages  */}
+								<Route path="/orders" element={<Orders />} />
+								<Route path="/employees" element={<Employees />} />
+								<Route path="/customers" element={<Customers />} />
 
-							{/* Apps */}
-							<Route path="/kanban" element={<Kanban />} />
-							<Route path="/editor" element={<Editor />} />
-							<Route path="/calender" element={<Calender />} />
-							<Route path="/colorpicker" element={<ColorPicker />} />
+								{/* apps  */}
+								<Route path="/kanban" element={<Kanban />} />
+								<Route path="/editor" element={<Editor />} />
+								<Route path="/calendar" element={<Calendar />} />
+								<Route path="/color-picker" element={<ColorPicker />} />
 
-							{/* Charts */}
-							<Route path="/line" element={<Line />} />
-							<Route path="/area" element={<Area />} />
-							<Route path="/bar" element={<Bar />} />
-							<Route path="/pie" element={<Pie />} />
-							<Route path="/financial" element={<Financial />} />
-							<Route path="/color-mapping" element={<ColorMapping />} />
-							<Route path="/pyramid" element={<Pyramid />} />
-							<Route path="/stacked" element={<Stacked />} />
-						</Routes>
+								{/* charts  */}
+								<Route path="/line" element={<Line />} />
+								<Route path="/area" element={<Area />} />
+								<Route path="/bar" element={<Bar />} />
+								<Route path="/pie" element={<Pie />} />
+								<Route path="/financial" element={<Financial />} />
+								<Route path="/color-mapping" element={<ColorMapping />} />
+								<Route path="/pyramid" element={<Pyramid />} />
+								<Route path="/stacked" element={<Stacked />} />
+							</Routes>
+						</div>
+						<Footer />
 					</div>
 				</div>
 			</BrowserRouter>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
 
-export default Footer
+const d = new Date()
+let currentYear = d.getFullYear()
+const Footer = () => (
+  
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © {currentYear} All rights reserved by Shoppy.com
+    </p>
+  </div>
+);
+
+export default Footer;
